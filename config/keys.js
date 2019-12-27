14b182e3ee81cdb6970118511a8c2313364c7c4b
AYUSH MISHRA <36783003+ayushmishra6198@users.noreply.github.com>
2019-12-27 11:36:50 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
 module.exports={
     google:{
-        clientId:"612163602836-qaob5jqui863hhih3faag4ba21h6ri43.apps.googleusercontent.com",
+        clientId:"612163602836-qaob5jqui863hh",
         clentSecret:"upY-r_UYh2Gt9O1CbQ64pJX0"
     },
     mongodb:{
-        dbURI:'mongodb+srv://admin:123456@Am@cluster0-wfehs.mongodb.net/test?retryWrites=true&w=majority'
+        dbURI:'mongodb+srv://admin:12345'
     },
     session:{
         cookieKey:'mishra is legend'
